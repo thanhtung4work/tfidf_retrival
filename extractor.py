6b8508be3cdee780246ac959a97cf1e7c49f56d2
tungt <tungt@uniquify.com>
2024-08-29 14:08:15 +0700

    preprocess data

--- extractor.py
+++ extractor.py
@@ -5,6 +5,8 @@ from   PyPDF2 import PdfReader
 from   sklearn.feature_extraction.text import TfidfVectorizer
 from   sklearn.metrics.pairwise import cosine_similarity
 
+import preprocessing
+
 documents = []
 files = os.listdir("documents")
 for filename in files:
@@ -13,15 +15,22 @@ for filename in files:
     text = ""
     for page in reader.pages:
         text += page.extract_text()
+    
+    text = preprocessing.to_lower(text)
+    text = preprocessing.remove_punc(text)
+    text = preprocessing.remove_whitespace(text)
     documents.append(text)
 
 vectorizer = TfidfVectorizer()
 tfidf_matrix = vectorizer.fit_transform(documents)
-numpy.save("retrival_matrix.npy", tfidf_matrix)
+numpy.save("retrival_matrix/retrival_matrix.npy", tfidf_matrix)
 
-# query = ["object detection"]
-# query_vec = vectorizer.transform(query)
-# similarities = cosine_similarity(query_vec, tfidf_matrix).flatten()
+while True:
+    q = input("what's your query? ")
+    if q == "quit": break
+    query = [q]
+    query_vec = vectorizer.transform(query)
+    similarities = cosine_similarity(query_vec, tfidf_matrix).flatten()
 
-# for file, score in zip(files, similarities):
-#     print(file, score)
+    for file, score in zip(files, similarities):
+        print(f"{file:<{24}}: {score}")
