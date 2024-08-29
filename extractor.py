d38852b8cf569644be5f25b7a09c932016ccf047
tungt <tungt@uniquify.com>
2024-08-29 16:53:59 +0700

    parse args

--- extractor.py
+++ extractor.py
@@ -1,4 +1,6 @@
+import argparse
 import os
+import shutil
 
 import numpy
 from   PyPDF2 import PdfReader
@@ -7,30 +9,52 @@ from   sklearn.metrics.pairwise import cosine_similarity
 
 import preprocessing
 
-documents = []
-files = os.listdir("documents")
-for filename in files:
-    reader = PdfReader(f"documents/{filename}")
+parser = argparse.ArgumentParser(
+    "Document extractor",
+    description="CLI application for managing, extracting, retrieving documents"
+)
 
-    text = ""
-    for page in reader.pages:
-        text += page.extract_text()
+parser.add_argument("--retrieve", type=bool, help="Retrieve query")
+parser.add_argument("--add-document", type=str, help="Add document to storage")
+
+
+def retrieve():
+    documents = []
+    files = os.listdir("documents")
+    for filename in files:
+        reader = PdfReader(f"documents/{filename}")
+
+        text = ""
+        for page in reader.pages:
+            text += page.extract_text()
+        
+        text = preprocessing.to_lower(text)
+        text = preprocessing.remove_punc(text)
+        text = preprocessing.remove_whitespace(text)
+        documents.append(text)
+
+    vectorizer = TfidfVectorizer()
+    tfidf_matrix = vectorizer.fit_transform(documents)
+    numpy.save("retrival_matrix/retrival_matrix.npy", tfidf_matrix)
+
+    while True:
+        q = input("what's your query? (type quit to stop) ")
+        if q == "quit": break
+        query = [q]
+        query_vec = vectorizer.transform(query)
+        similarities = cosine_similarity(query_vec, tfidf_matrix).flatten()
+
+        for file, score in zip(files, similarities):
+            print(f"{file:<{24}}: {score}")
+
+def add_document(filename: str):
+    shutil.copyfile(filename, f"documents/{os.path.basename(filename)}")
+
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    if args.add_document:
+        add_document(args.add_document)
     
-    text = preprocessing.to_lower(text)
-    text = preprocessing.remove_punc(text)
-    text = preprocessing.remove_whitespace(text)
-    documents.append(text)
-
-vectorizer = TfidfVectorizer()
-tfidf_matrix = vectorizer.fit_transform(documents)
-numpy.save("retrival_matrix/retrival_matrix.npy", tfidf_matrix)
-
-while True:
-    q = input("what's your query? ")
-    if q == "quit": break
-    query = [q]
-    query_vec = vectorizer.transform(query)
-    similarities = cosine_similarity(query_vec, tfidf_matrix).flatten()
-
-    for file, score in zip(files, similarities):
-        print(f"{file:<{24}}: {score}")
+    if args.retrieve:
+        retrieve()
