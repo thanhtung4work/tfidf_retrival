7a16b447cc6cfe7fb465015d41ca06c9a4a72af6
tungt <tungt@uniquify.com>
2024-09-05 15:32:03 +0700

    add cli

--- cli.py
+++ cli.py
@@ -8,6 +8,10 @@ from   sklearn.feature_extraction.text import TfidfVectorizer
 from   sklearn.metrics.pairwise import cosine_similarity
 
 import preprocessing
+from   utils import save_uploaded_file, extract_document
+
+# Configure upload folder
+UPLOAD_FOLDER = 'documents/'
 
 parser = argparse.ArgumentParser(
     "Document extractor",
@@ -19,33 +23,14 @@ parser.add_argument("--add-document", type=str, help="Add document to storage")
 
 
 def retrieve():
-    documents = []
-    files = os.listdir("documents")
-    for filename in files:
-        reader = PdfReader(f"documents/{filename}")
-
-        text = ""
-        for page in reader.pages:
-            text += page.extract_text()
-        
-        text = preprocessing.to_lower(text)
-        text = preprocessing.remove_punc(text)
-        text = preprocessing.remove_whitespace(text)
-        documents.append(text)
-
-    vectorizer = TfidfVectorizer()
-    tfidf_matrix = vectorizer.fit_transform(documents)
-    numpy.save("retrival_matrix/retrival_matrix.npy", tfidf_matrix)
-
     while True:
         q = input("what's your query? (type quit to stop) ")
         if q == "quit": break
-        query = [q]
-        query_vec = vectorizer.transform(query)
-        similarities = cosine_similarity(query_vec, tfidf_matrix).flatten()
-
-        for file, score in zip(files, similarities):
-            print(f"{file:<{24}}: {score}")
+        sim_index = extract_document(
+            q,
+            UPLOAD_FOLDER
+        )
+        print(sim_index)
 
 def add_document(filename: str):
     shutil.copyfile(filename, f"documents/{os.path.basename(filename)}")
